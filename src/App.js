215a73c4270f31d7c91cdfda73766af8ee1dfe9a
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2021-10-13 11:23:21 -0300

    deploy trivia-react-redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
@@ -8,13 +8,15 @@ import Ranking from './pages/Ranking';
 
 function App() {
   return (
-    <Switch>
-      <Route path="/game" component={ Game } />
-      <Route path="/settings" component={ Settings } />
-      <Route path="/feedback" component={ Feedback } />
-      <Route path="/ranking" component={ Ranking } />
-      <Route exact path="/" component={ Login } />
-    </Switch>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route path="/game" component={ Game } />
+        <Route path="/settings" component={ Settings } />
+        <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </Router>
   );
 }
 
